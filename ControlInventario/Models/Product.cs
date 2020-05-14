6d1455e1e813a6f9bf862462e60b0b4a5e13f86a
RodrigoMS95-ITT <rodrigo.murguia17@tectijuana.edu.mx>
2020-05-13 20:08:03 -0700

    P04
    Subida de practica tarea

---
+++ ControlInventario/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlTypes;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControlInventario.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Display(Name="Codigo")]        
+        public string ProductCode { get; set; }
+        [Display(Name = "Nombre")]
+        public string ProductName { get; set; }
+        [Display(Name = "Descripción")]
+        public string Description { get; set; }
+        [Display(Name = "Cantidad")]
+        public int Quantity { get; set; }
+        [Display(Name = "Precio")]
+        public decimal Price { get; set; }
+        public int SupplirId { get; set; }
+        [ForeignKey("SupplirId")]
+        public Supplier Supplier { get; set; }
+
+    }
+}
---
+++ ControlInventario/Models/Supplier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ControlInventario.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        [Display(Name = "Codigo proveedor")]
+        public string SupplierCode { get; set; }
+        public string SupplierName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        
+    }
+}
